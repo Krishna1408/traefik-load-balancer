9163c388d2db8a87ee5c490352e39d856a64d1a1
docerrasible <krishna.sharma1408@gmail.com>
2019-02-10 11:46:47 +0100

    Update dump.sql

--- sql/dump.sql
+++ sql/dump.sql
@@ -1,13 +1,13 @@
-create TABLE falconi.users (
+create TABLE traefik.users (
  id int(8) unsigned zerofill not null auto_increment primary key,
  name varchar(30) not null
 );
-INSERT INTO falconi.users (name) VALUES ('user1');
-INSERT INTO falconi.users (name) VALUES ('user2');
-INSERT INTO falconi.users (name) VALUES ('user3');
-INSERT INTO falconi.users (name) VALUES ('user4');
-INSERT INTO falconi.users (name) VALUES ('user5');
-INSERT INTO falconi.users (name) VALUES ('user6');
-INSERT INTO falconi.users (name) VALUES ('user7');
+INSERT INTO traefik.users (name) VALUES ('user1');
+INSERT INTO traefik.users (name) VALUES ('user2');
+INSERT INTO traefik.users (name) VALUES ('user3');
+INSERT INTO traefik.users (name) VALUES ('user4');
+INSERT INTO traefik.users (name) VALUES ('user5');
+INSERT INTO traefik.users (name) VALUES ('user6');
+INSERT INTO traefik.users (name) VALUES ('user7');
 
 
